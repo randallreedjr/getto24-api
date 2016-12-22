c261b0bc3f8cdc2fce491e97ac5e7af47c9c98c5
Randall Reed, Jr <randallreedjr@gmail.com>
2016-12-21 22:54:53 -0500

    Migrate to Math24 gem v 2.0.0

--- spec/controllers/problem_controller_spec.rb
+++ spec/controllers/problem_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe ProblemController, type: :controller do
     it "generates a problem with four single digit numbers" do
       get :generate
       problem = JSON.parse(response.body)["problem"]
-      number_count = problem.count {|i| /\d{1}/.match(i) }
+      number_count = problem.count {|i| /\d{1}/.match(i.to_s) }
       expect(problem.length).to eq(4)
       expect(number_count).to eq(4)
     end
